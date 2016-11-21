70eb57e02e4648633cd428b8fe4dd93ba0aedd5d
m00024 <cesium_bang@yahoo.co.uk>
2016-11-21 14:56:20 +0000

    Update app.js

--- TheNextPage/app.js
+++ TheNextPage/app.js
@@ -1,1 +1,21 @@
 
+var main = function()
+{
+  draw = function()
+  {
+      stroke(255,255,255);
+      //noStroke();
+      //stroke(mouseX,mouseY,(mouseY-mouseX));
+      fill(mouseY, mouseX, (mouseX-mouseY));
+      
+      if (mouseIsPressed)
+      {
+       ellipse(mouseX, mouseY, mouseX, mouseY);
+      }
+      else
+      {
+       rect(mouseX-(mouseX/2),mouseY-(mouseY/2),mouseX,mouseY);
+      }
+   };
+};
+$(document).ready(main);
